c695a47b17ac59bc68e81d391bd4461e571789d1
Glorings <18668465735@163.com>
2019-07-11 00:05:15 +0800

    init

---
+++ src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,34 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+
+    /**
+     * 基本的转发
+     * 当访问http://localhost:8080/jd
+     * 转发到http://jd.com
+     * @param builder
+     * @return
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                //basic proxy
+                .route(r ->
+                        r.path("/jd")
+                        .uri("http://jd.com:80/")
+                        .id("jd_route")
+                ).build();
+    }
+}
